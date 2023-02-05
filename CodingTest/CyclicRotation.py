ecbc1467f356031e06b16f1c6c48ba7f76c47e86
류은지 <51250746+ChloeeeYoo@users.noreply.github.com>
2023-02-05 23:22:54 +0900

    Update CyclicRotation.py

--- CodingTest/CyclicRotation.py
+++ CodingTest/CyclicRotation.py
@@ -1,4 +1,7 @@
-def ShiftRight(K, N, i):
+############# Version 1 ##############################
+######################################################
+
+def ShiftRight(K, N, i):        # Just directly count the final index and array at once 
     K = K % N
     new_pos = i + K
     if (new_pos >= N):
@@ -7,14 +10,42 @@ def ShiftRight(K, N, i):
     return new_pos
 
 def solution(A, K):
-    # Implement your solution here
+   
     N = len(A)
     final_A = []
 
     for i in range(N):
-        new_pos = ShiftRight(K,N,i)
+        new_pos = ShiftRight(K,N,i). # i is index in array
         final_A.insert(new_pos, A[i])
         #print(A[i])
         #print(final_A[i])
     
     return final_A
+
+######################################################
+
+
+############# Version 2 ##############################
+######################################################
+
+def ShiftRight(A, N):    # for just one shifting to right way
+    shift_A = []
+    for n in range(N):
+        if(n+1 == N):
+            shift_A.insert(0, A[n])
+        else:
+            shift_A.insert(n+1, A[n])
+
+    return shift_A
+
+def solution(A, K):
+    
+    N = len(A)
+    Result = A
+
+    for k in range(K):     # Until the input K counts
+        Result = ShiftRight(Result, N).copy()
+    
+    return Result
+
+######################################################
