8a9f5401b17a43b8d1586b0e6c06f72f9a175253
류은지 <51250746+ChloeeeYoo@users.noreply.github.com>
2023-02-05 21:43:06 +0900

    Create CyclicRotation.py

---
+++ CodingTest/CyclicRotation.py
@@ -0,0 +1,20 @@
+def ShiftRight(K, N, i):
+
+    new_pos = i + K
+    if (new_pos >= N):
+        new_pos = new_pos - N
+    
+    return new_pos
+
+def solution(A, K):
+    # Implement your solution here
+    N = len(A)
+    final_A = []
+
+    for i in range(N):
+        new_pos = ShiftRight(K,N,i)
+        final_A.insert(new_pos, A[i])
+        print(A[i])
+        print(final_A[i])
+    
+    return final_A
