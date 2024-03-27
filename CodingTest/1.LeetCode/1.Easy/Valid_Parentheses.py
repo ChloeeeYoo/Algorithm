48796e8234760b746502c9d15b8c3bd3f3fce7d2
류은지 <51250746+ChloeeeYoo@users.noreply.github.com>
2024-03-27 09:29:15 +0900

    Update Valid_Parentheses.py

--- CodingTest/1.LeetCode/1.Easy/Valid_Parentheses.py
+++ CodingTest/1.LeetCode/1.Easy/Valid_Parentheses.py
@@ -5,14 +5,14 @@ class Solution:
             if val in '({[':        # Check whether it is opening bracket
                 mem.append(val)      # -> stack the parameter into the stack list
             elif val in ')}]':      # Check whether it is closing bracket
-                if not mem:          # It is 'False' when getting the closing bracket first without getting the opening bracket 
+                if not mem:          # It is 'False' when list is empty
                     return False
                 elif 1 <= ord(val) - ord(mem[-1]) <= 2:    # using the ascii code value / all substract opening and closing bracket is within 1~2 
                     mem.pop()                              # pop the opening bracket when it is matching 
                     continue
                 else:                                      # it is not matching the opening and closing bracket
                     return False
-        if not mem:                                        # the number of opening and closing bracket match
+        if not mem:                     # list is empty    # the number of opening and closing bracket match
             return True
         else:                                              # the number of opening and closing bracket does not match
             return False
